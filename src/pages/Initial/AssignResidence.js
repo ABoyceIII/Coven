d844e421f2d2b4b10516977373deceac54885968
AllenBoyce <allenboyceiii@gmail.com>
2024-07-02 15:37:36 -0700

    added name fields in residence assigning page

--- src/pages/Initial/AssignResidence.js
+++ src/pages/Initial/AssignResidence.js
@@ -3,6 +3,9 @@ import { useState } from "react";
 export default function AssignResidence(props) {
   const [isJoining, setIsJoining] = useState(false);
   const [isCreating, setIsCreating] = useState(false);
+
+  const [fullName, setFullName] = useState("");
+  const [displayName, setDisplayName] = useState("");
   return (
     <div className="AssignResidence">
       <h2>would you like to create a residence or join an existing one?</h2>
