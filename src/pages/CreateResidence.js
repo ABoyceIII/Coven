fa9661dc173e5b5ac4f9e7f5423a0d2eef11cabc
AllenBoyce <allenboyceiii@gmail.com>
2024-08-29 15:02:20 -0700

    added create and back buttons, implemented back button

--- src/pages/CreateResidence.js
+++ src/pages/CreateResidence.js
@@ -3,6 +3,9 @@ import { useState } from "react";
 import "../css/CreateResidence.css";
 export default function CreateResidence(props) {
   const [name, setName] = useState("");
+
+  //TODO: Implement sase of no user
+
   return (
     <div className="CreateResidence">
       <h2>what would you like to call your new home?</h2>
@@ -11,6 +14,14 @@ export default function CreateResidence(props) {
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
+      <button>create</button>
+      <button
+        onClick={() => {
+          window.location.href = "/account";
+        }}
+      >
+        back
+      </button>
     </div>
   );
 }
